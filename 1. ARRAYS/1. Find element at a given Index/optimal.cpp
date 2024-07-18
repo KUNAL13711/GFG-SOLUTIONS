87cacfa245eae7922896782bc5f8d016aab8e57a
KUNAL13711 <98326045+KUNAL13711@users.noreply.github.com>
2024-07-18 11:10:10 +0530

    Create optimal.cpp
    Optimal solution of Find element at a given Index

---
+++ 1. ARRAYS/1. Find element at a given Index/optimal.cpp
@@ -0,0 +1,10 @@
+class Solution {
+  public:
+    int findElementAtIndex(int key, vector<int> &arr)
+    {
+        /*--- OPTIMAL:
+        T.C: O(1)
+        S.C: O(1) ---*/
+        return (arr[key]);
+    }
+};
